b34790a50220494bfe4893d2d730da35d0449f22
Rahul <rahullama34@gmail.com>
2024-12-18 09:52:57 +0530

    SearchBar

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { PlatformIconList } from "./PlatformIconList";
 import MetacriticScore from "./MetacriticScore";
+import NoImage from "../assets/No_Image_Placeholder.png";
 
 export const GameCard = ({ game }) => {
   const getCroppedImage = (url) => {
-    if (!url) return;
+    if (!url) return NoImage;
     const index = url.indexOf("media/") + "media/".length;
     return url.slice(0, index) + "crop/600/400/" + url.slice(index);
   };
@@ -13,7 +14,7 @@ export const GameCard = ({ game }) => {
     <>
       <div className="bg-white rounded-lg dark:bg-gray-800 shadow-2xl">
         <img
-          className="rounded-t-lg overflow-hidden cursor-pointer hover:brightness-75"
+          className="rounded-t-lg w-full  overflow-hidden cursor-pointer hover:brightness-75"
           src={getCroppedImage(game.background_image)}
           alt={game.name}
         />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { ColorModeSwitch } from "./ColorModeSwitch";
+import { SearchInput } from "./SearchInput";
 
 export const NavBar = () => {
   return (
-    <div className="flex justify-between w-full">
+    <div className="flex justify-between w-full items-center">
       <span className="font-bold">GameHub</span>
-      <ColorModeSwitch />
+      <div className="flex space-x-6 ml-6">
+        <SearchInput />
+        <ColorModeSwitch />
+      </div>
     </div>
   );
 };
